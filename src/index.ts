f84f2aabddbb6443f78296c9e48d87faa35ec166
Dmitry <dmitry27zh@gmail.com>
2024-01-06 12:37:00 +0500

    Add new task

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-// import { v4 as uuidV4 } from 'uuid';
-
-// console.log(uuidV4());
+import { v4 as uuidV4 } from 'uuid';
 
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.querySelector<HTMLFormElement>('#new-task-form');
 const input = document.getElementById(
-  'new-task-input',
+  'new-task-title',
 ) as HTMLInputElement | null;
 
 form?.addEventListener('submit', (e) => {
@@ -14,4 +12,34 @@ form?.addEventListener('submit', (e) => {
   if (input?.value === '' || input?.value == null) {
     return;
   }
+
+  const task: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  addNewTask(task);
 });
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+function addNewTask(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
